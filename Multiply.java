6ec44bcdd7462ab68483999fff7a1ef146def052
Aditya Mittal <aditya.mittal777@gmail.com>
2021-11-02 16:42:47 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    
+    public static void main(String[] args){
+        int a = 10;
+        int b = 30;
+        System.out.println("The product is ::"+(a*b));
+    }
+}
